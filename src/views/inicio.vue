3d51adb15c5381a730e9ea8e43137ebd12cceea2
Duglas <duglasphone@gmail.com>
2023-07-07 22:38:19 -0500

    update

--- src/views/inicio.vue
+++ src/views/inicio.vue
@@ -25,3 +25,57 @@
         </article>
     </div>
 </template>
+
+<style>
+*{
+    font-family: sans-serif;
+}
+body{
+    margin: 0%;
+}
+nav{
+    padding-top: 2px;
+    margin-top: 0px;
+    background-color: #933A39;
+    height: 100px;
+}
+a{  display: flex;
+    flex-direction: column;
+    justify-content: center;
+    flex-shrink: 0;
+    margin-top: 15px;
+    margin-right: 20px;
+    text-align: center;
+    float: right;
+    color: white;
+    width: 135px;
+    height: 68px;
+    border-radius: 40px;
+    background: #583939;
+    font-size: 18px;
+}
+a:link, a:visited, a:active { text-decoration:none; }
+
+article{
+    padding: 15px;
+    margin: 20px;
+    float: left;
+    display: flex;
+    width: 20%;
+    height: 20%;
+    flex-direction: column;
+    justify-content: center;
+    flex-shrink: 0;
+    border-radius: 25px;
+    border-color: #583939;
+    border: solid 1px;
+}
+img{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    border-radius: 20px;
+    width: 150px;
+    height: 150px;
+    text-align: center;
+}</style>
